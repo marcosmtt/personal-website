376eba9afc1c0b6bf5f3b3bcf9fd753de0b6a10c
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-03-19 22:02:47 -0300

    Add mobile drop down menu

---
+++ scripts/functions.js
@@ -0,0 +1,11 @@
+function dropMenu() {
+
+    if (document.getElementById("dropContent").style.display == 'inline') {
+        document.getElementById("dropContent").style.display = 'none';
+        document.getElementById("menuButton").style.color = 'white';
+    } else {
+        document.getElementById("dropContent").style.display = 'inline';
+        document.getElementById("menuButton").style.color = '#0a0a11';
+    }
+
+};
